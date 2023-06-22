fc639c12601ceb3d7913080cf3f320c0cfefff97
Vlad <vla.koncha@gmail.com>
2023-06-22 20:53:25 +0700

    adds TicketCard

---
+++ biletopoisk-app/src/components/ticket-card/TicketCard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import classes from "./TicketCard.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import TicketClicker from "@/components/ticket-clicker/TicketClicker";
+import { Movie } from "@/api-types/types";
+import { genreMap } from "@/utils/genreMap";
+
+interface Props {
+  movie: Movie;
+}
+
+const TicketCard = async ({ movie }: Props) => {
+  return (
+    <div className={classes.cardContainer}>
+      <div className={classes.imageContainer}>
+        <Image
+          alt={`Постер для ${movie.title}`}
+          src={movie.posterUrl}
+          loading="lazy"
+          width={450}
+          height={300}
+          fill={false}
+        />
+      </div>
+
+      <div className={classes.descriptionContainer}>
+        <Link href="/">
+          <p className={classes.titleText}>{movie.title}</p>
+        </Link>
+        <p className={classes.genreText}>{genreMap[movie.genre]}</p>
+      </div>
+
+      <div className={classes.clickerContainer}>
+        <TicketClicker />
+      </div>
+    </div>
+  );
+};
+
+export default TicketCard;
